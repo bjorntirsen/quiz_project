5dbbf31deff674722ea4e9ea9030a049aec9e828
Björn Tirsén <bjorntirsen@gmail.com>
2020-09-30 21:35:48 +0200

    Developed classes question_list and question

---
+++ js/question.js
@@ -0,0 +1,15 @@
+class Question {
+    constructor(){
+        this.question = "";
+        this.correct_answer = "";
+        this.answer_a = null;
+        this.answer_b = null;
+        this.answer_c = null;
+        this.answer_d = null;
+        this.answer_e = null;
+        this.answer_f = null;
+    }
+    correct(){
+
+    }
+}
--- js/question_list.js
+++ js/question_list.js
@@ -1,30 +1,31 @@
-/* class Question_list {
+class Question_list {
     constructor(){
         this.question_amount = sessionStorage.getItem("question_amount");          
         let list = [];
-        for (let i = 0; i<this.question_amount; i++) {
-
-        }
+        this.fetchQuestions();
     }
     fetchQuestions() {
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then(data => {
-        let first = data.results[0].name.first;
-        let last = data.results[0].name.last;
-        let email = data.results[0].email;
-        let randomPerson = new RandomPerson(first, last, email);
-        list.addPerson(randomPerson);
-    
-    })
+        fetch("https://quizapi.io/api/v1/questions?category=code&limit="+this.question_amount, {
+            headers: {
+                "X-Api-Key": "IfB2IekyprvvccI060Y5FwUAkczl7pI2CFVJKzhZ",
+            }
+        })
+        .then((response) => response.json())
+        .then((json) => {
+            for (let i = 0; i < this.question_amount; i++){
+                let question = new Question();
+                question.question = json[i].question;
+                question.correct_answer = json[i].correct_answer;
+                question.answer_a = json[i].answer_a;
+                question.answer_b = json[i].answer_b;
+                question.answer_c = json[i].answer_c;
+                question.answer_d = json[i].answer_d;
+                question.answer_e = json[i].answer_e;
+                question.answer_f = json[i].answer_f;
+            }
+        });
     }
-} */
-ten questions
-https://quizapi.io/api/v1/questions?apiKey=YOUR_API_KEY&category=code&difficulty=Easy&limit=10
-
-five questions
-https://quizapi.io/api/v1/questions?apiKey=YOUR_API_KEY&category=code&difficulty=Easy&limit=5
-
+}
 
 fetch("https://randomuser.me/api/")
 .then(response => response.json())
