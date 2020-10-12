72d9bd532f7b173411d8c08c9625800f6defd623
Björn Tirsén <bjorntirsen@gmail.com>
2020-10-12 09:58:23 +0200

    Added try-catch to fetch

--- js/question_list.js
+++ js/question_list.js
@@ -7,37 +7,42 @@ class Question_list {
     //This is the method that fetches the questions from the API
     async fetchQuestions(question_amount, main_object) {
         let list = this.list;
-        await fetch("https://quizapi.io/api/v1/questions?category=code&limit="+question_amount, {
-            headers: {
-                "X-Api-Key": "IfB2IekyprvvccI060Y5FwUAkczl7pI2CFVJKzhZ",
-            }
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            for (let i = 0; i < this.question_amount; i++){
-                //Turning the response from the API into question objects
-                let question = new Question();
-                //Here we are taking the key-value pairs of answers and
-                //inserting them in the answers array of the object.
-                //Like this: [ 'answer_a', 'ceil(x, y)' ]
-                question.answers = Object.keys(data[i].answers).map((key) => [key, data[i].answers[key]]);
-                //We then do the same for the correct answers
-                question.correct_answers = Object.keys(data[i].correct_answers).map((key) => [data[i].correct_answers[key]]);
-                //But here we are also then converting that array into
-                //an array of booleans.
-                //Like this: [ false, true, false, false, false, false ]
-                question.correct_answers = question.correct_answers.map((val) => (val == "true"));
-                //Both of the above arrays will have the length 6.
-                //Below its just a single string we are adding each time.
-                question.question = data[i].question;
-                question.tag = data[i].tags[0].name;
-                question.difficulty = data[i].difficulty;
-                //Finally we are pushing the question object into our list array.
-                list.push(question);
-            }  
-        });
-        main_object.initializeNextButton();
-        document.getElementById("fetching").classList.add("invisible");        
+        try {        
+            await fetch("https://quizapi.io/api/v1/questions?category=code&limit="+question_amount, {
+                headers: {
+                    "X-Api-Key": "IfB2IekyprvvccI060Y5FwUAkczl7pI2CFVJKzhZ",
+                }
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                for (let i = 0; i < this.question_amount; i++){
+                    //Turning the response from the API into question objects
+                    let question = new Question();
+                    //Here we are taking the key-value pairs of answers and
+                    //inserting them in the answers array of the object.
+                    //Like this: [ 'answer_a', 'ceil(x, y)' ]
+                    question.answers = Object.keys(data[i].answers).map((key) => [key, data[i].answers[key]]);
+                    //We then do the same for the correct answers
+                    question.correct_answers = Object.keys(data[i].correct_answers).map((key) => [data[i].correct_answers[key]]);
+                    //But here we are also then converting that array into
+                    //an array of booleans.
+                    //Like this: [ false, true, false, false, false, false ]
+                    question.correct_answers = question.correct_answers.map((val) => (val == "true"));
+                    //Both of the above arrays will have the length 6.
+                    //Below its just a single string we are adding each time.
+                    question.question = data[i].question;
+                    question.tag = data[i].tags[0].name;
+                    question.difficulty = data[i].difficulty;
+                    //Finally we are pushing the question object into our list array.
+                    list.push(question);
+                }  
+            });
+            main_object.initializeNextButton();
+            document.getElementById("fetching").classList.add("invisible");
+        }
+        catch (error) {
+            alert("I was not able to get the questions. Please check your browser's internet connection and then reload this page.")
+        }    
     }
 
     //See if there were multiple answers in the quiz
